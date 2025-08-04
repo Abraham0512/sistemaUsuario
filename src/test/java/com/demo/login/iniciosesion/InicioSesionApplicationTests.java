e9f577c80c08fb7782d66ccf504819680ea27c91
jvargasp <jvargasp@inss.gob.ni>
2025-08-04 11:14:52 -0600

    feat: se agrego la configuracion del proyecto

---
+++ src/test/java/com/demo/login/iniciosesion/InicioSesionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.login.iniciosesion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicioSesionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
